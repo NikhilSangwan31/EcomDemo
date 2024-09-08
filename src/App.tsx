473fe02d7b90a05d831b4f8fb0ca3efd16b2e71b
NikhilleadingDots <nikhil@leadingdots.com>
2024-09-08 14:23:19 +0530

    Initial code
    Typescript Redux toolkit code or Ecommerce Demo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import AppRouter from "./routes/routes";
+
+
+function App() {
+  return (
+    <>
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
